027cd50f34cc6cce94ae49f7b712b7af6ef00806
한규범 <hgb@quantec.co.kr>
2024-12-10 13:52:15 +0900

    refactor: Modify preprocessing_data func for cal ROIC, feat: add rank_data func

--- test_package/preprocessing.py
+++ test_package/preprocessing.py
@@ -1,8 +1,23 @@
 import pandas as pd
 
 
-def preprocessing_data(df: pd.DataFrame):
+def preprocessing_data(df: pd.DataFrame) -> pd.DataFrame:
     df["SPS"] = df["Total Revenue"] / df["Number Of Shares"]
     df["Market Cap"] = df["Number Of Shares"] * df["close"]
+    df["ROIC"] = df["Net Operating Profit After Tax"] / df["Invested Capital"]
+
+    return df
+
+
+def rank_data(df: pd.DataFrame) -> pd.DataFrame:
+    df["PSR_rank"] = df.groupby(by="date")["PSR"].rank(ascending=True, method="first")
+    df["ROIC_rank"] = df.groupby(by="date")["ROIC"].rank(
+        ascending=False, method="first"
+    )
+
+    df["total_rankvalue"] = df["PSR_rank"] + df["ROIC_rank"]
+    df["total_rank"] = df.groupby(by="date")["total_rankvalue"].rank(
+        ascending=True, method="first"
+    )
 
     return df
