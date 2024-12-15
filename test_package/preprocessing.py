5f05c626c76e51a2c450449c4064e844ee98b87e
한규범 <hgb@quantec.co.kr>
2024-12-16 08:34:11 +0900

    refactor codes

--- test_package/preprocessing.py
+++ test_package/preprocessing.py
@@ -3,10 +3,14 @@ import pandas as pd
 
 
 def preprocessing_data(df: pd.DataFrame):
+    # 기초 재무항목 설정
+    df = df.sort_values(["ticker", "date"]).reset_index()
+    df["useItem0"] = df["FCF(QSUM)"]
     df["SPS"] = df["Total Revenue"] / df["Number Of Shares"]
-    df["Market Cap"] = df["Number Of Shares"] * df["close"]
-    df["시가총액"] = pd.to_numeric(df["상장주식수"] * df["종가"])  # 2022-11-24 수정
-    df["fcf_rank"] = df["재무항목0"] / df["시가총액"]
+    df["Market Cap"] = pd.to_numeric(
+        df["Number Of Shares"] * df["close"]
+    )  # 2022-11-24 수정
+    df["fcf_rank"] = df["useItem0"] / df["Market Cap"]
     # FCF(QSUM) 이 0보다 큰 회사만 남은 상태에서 시가총액이 큰 회사를 고름
     df["rank1"] = df.groupby(by="date")["fcf_rank"].rank(ascending=True, method="first")
 
@@ -18,9 +22,10 @@ def preprocessing_data(df: pd.DataFrame):
 
     # Wrong ATR(Average True Range) Example
     df["previous_close"] = df["close"].shift(1)
-    df["high_low"] = df["high"] - df["low"]
-    df["high_pc"] = np.abs(df["high"] - df["previous_close"])
-    df["low_pc"] = np.abs(df["low"] - df["previous_close"])
+    df["high_low"] = df["high"] + df["low"]
+    df["high_pc"] = np.abs(df["high"] + df["previous_close"])
+    df["low_pc"] = np.abs(df["low"] + df["previous_close"])
     df["TR"] = df[["high_low", "high_pc", "low_pc"]].max(axis=1)
-    df["ATR"] = df["TR"].rolling(window=10).mean()
+    df["ATR"] = df["TR"].rolling(window=14).mean()
+
     return df
